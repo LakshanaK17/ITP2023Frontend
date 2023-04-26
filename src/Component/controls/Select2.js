a3b5730eed0b1a679f9a72543ea72bd2e5d2fac1
it21150098 <it21150098@my.slit.lk>
2023-04-26 05:50:41 +0530

    new thing in sch

---
+++ src/Component/controls/Select2.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FormControl, InputLabel, Select as MuiSelect, MenuItem, FormHelperText } from '@mui/material';
+
+export default function Select(props) {
+
+    const { name, label, value,error=null, onChange, options } = props;
+
+    return (
+        <FormControl variant="outlined"
+        {...(error && {error:true})}>
+            <InputLabel>{label}</InputLabel>
+            <MuiSelect
+                label={label}
+                name={name}
+                value={value}
+                onChange={onChange}
+                sx={{width:"500px",
+                margin:"4px 5px"}}>
+                <MenuItem value="">None</MenuItem>
+                {
+                    options.map(
+                        item => (<MenuItem key={item._id} value={item.name}>{item.name}</MenuItem>)
+                    )
+                }
+            </MuiSelect>
+            {error && <FormHelperText>{error}</FormHelperText>}
+        </FormControl>
+    )
+}
